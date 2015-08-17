fd2f3e6bd35c8bd7f3b7b28a75199f63f19be26d
cosmicy <cosmicy@outlook.com>
2015-08-17 11:20:57 +0800

    添加BaiduMap 显示一个Marker

--- src/com/example/maptest/MainActivity.java
+++ src/com/example/maptest/MainActivity.java
@@ -1,16 +1,69 @@
 package com.example.maptest;
 
 import android.app.Activity;
+import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.BitmapDescriptor;
+import com.baidu.mapapi.map.BitmapDescriptorFactory;
+import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.Marker;
+import com.baidu.mapapi.map.MarkerOptions;
+import com.baidu.mapapi.map.OverlayOptions;
+import com.baidu.mapapi.model.LatLng;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 
+	MapView mMapView = null;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+
+		// 在使用SDK各组件之前初始化context信息，传入ApplicationContext
+		// 注意该方法要再setContentView方法之前实现
+		SDKInitializer.initialize(getApplicationContext());
+
 		setContentView(R.layout.activity_main);
+
+		// 获取地图控件引用
+		mMapView = (MapView) findViewById(R.id.bmapView);
+
+		BaiduMap _mapCtrl = mMapView.getMap();
+
+
+		/////////////////////
+		//地图api测试
+		LatLng llA = new LatLng(39.906965, 116.401394);
+		Marker mMarkerA;
+		BitmapDescriptor icon = BitmapDescriptorFactory.fromResource(R.drawable.ic_launcher);
+		OverlayOptions ooA = new MarkerOptions().position(llA).icon(icon)
+				.zIndex(9).draggable(true);
+		mMarkerA = (Marker) (_mapCtrl.addOverlay(ooA));
+
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		// 在activity执行onDestroy时执行mMapView.onDestroy()，实现地图生命周期管理
+		mMapView.onDestroy();
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		// 在activity执行onResume时执行mMapView. onResume ()，实现地图生命周期管理
+		mMapView.onResume();
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		// 在activity执行onPause时执行mMapView. onPause ()，实现地图生命周期管理
+		mMapView.onPause();
 	}
 
 	@Override
